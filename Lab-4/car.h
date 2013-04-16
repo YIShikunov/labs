cf56f3c1ff389f7d923c579cdaf0cc9aebb51013
YI Shikunov <YIShikunov@gmail.com>
2013-04-16 16:12:38 +0400

    Finished Lab-4

--- Lab-4/car.h
+++ Lab-4/car.h
@@ -1,11 +1,44 @@
 class Car
 {
-private:
-
+protected:
+	int speed;
+	char* name;
 public:
-
+	Car()
+	{
+		name = new char[0];
+		speed = 0;
+	}
+	Car(char * name, int speed)
+	{
+		this->name = name;
+		this->speed = speed;
+	}
+	void virtual printInfo()
+	{
+		std::cout<<"Name: "<<name<<std::endl<<"Speed: "<<speed<<std::endl;
+	}
 };
 class Bus : public Car
 {
-
+private:
+	int passengers;
+public:
+	Bus()
+	{
+		name = new char[0];
+		speed = 0;
+		passengers = 0;
+	}
+	Bus(char * name, int speed, int maxPassengers)
+	{
+		this->name = name;
+		this->speed = speed;
+		this->passengers = maxPassengers;
+	}
+	void printInfo()
+	{
+		Car::printInfo();
+		std::cout<<"Max Passengers: "<<passengers<<std::endl;
+	}
 };
--- Lab-4/main.cpp
+++ Lab-4/main.cpp
@@ -3,4 +3,13 @@
 
 void main()
 {
+	Car * c = new Car("Volga", 5);
+	Bus * b = new Bus("Ikarus", 45, 20);
+
+	c->printInfo();
+	b->printInfo();
+
+	c = new Bus("Mercedes", 150, 50);
+
+	c->printInfo();
 }
